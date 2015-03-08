07b04b443038bd64af458ce0e4279c12d01cd8a3
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-03-08 19:40:25 +0900

    マクロを充実

--- ShireiKantai/ShireiKantai/PropertyMacro.hpp
+++ ShireiKantai/ShireiKantai/PropertyMacro.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+// プロパティを指定した型で展開する
 #define PROPERTY(TYPE, VAR) \
 	private: \
 		TYPE VAR; \
@@ -8,6 +9,7 @@
 		void __Set##VAR(const TYPE& value) { VAR = value; } \
 		__declspec(property(get = __Get##VAR, put = __Set##VAR)) TYPE VAR;
 
+// shared_ptr版
 #define PROPERTY_S(TYPE, VAR) \
 	private: \
 		std::shared_ptr<TYPE> VAR; \
@@ -15,3 +17,12 @@
 		TYPE __Get##VAR() const { return *VAR; } \
 		void __Set##VAR(const TYPE& value) { *VAR = value; } \
 		__declspec(property(get = __Get##VAR, put = __Set##VAR)) TYPE VAR;
+
+#define P_INTEGER(VAR) \
+	VAR = json[#VAR].int_value();
+
+#define P_STRING(VAR) \
+	VAR = Widen(json[#VAR].string_value());
+
+#define P_SHIGEN(VAR) \
+	VAR = Shigen(json[#VAR]);
