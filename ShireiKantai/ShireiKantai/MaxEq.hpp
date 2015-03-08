137f459d120afbd213c63fcb2111b6c715b48883
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-03-08 20:16:36 +0900

    MaxEqを追加

---
+++ ShireiKantai/ShireiKantai/MaxEq.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#pragma once
+
+#include <json11.hpp>
+#include <vector>
+#include <Siv3D.hpp>
+
+namespace kantai
+{
+	namespace data
+	{
+		/*
+		* 初期装備を表すクラス
+		*/
+		class MaxEq
+		{
+			std::array<int, 5> Slots;
+
+		public:
+			/*
+			* 初期装備を表すクラス
+			*/
+			MaxEq() { }
+
+			/*
+			* 初期装備を表すクラス
+			*/
+			MaxEq(const std::array<int, 5>& equips) { Slots = equips; }
+
+			/*
+			* 初期装備を表すクラス
+			*/
+			MaxEq(const json11::Json& json)
+			{
+				const auto items = json.array_items();
+				for (int i = 0; i < items.size(); ++i)
+				{
+					Slots[i] = items[i].int_value();
+				}
+			}
+
+			MaxEq& operator=(const MaxEq& obj)
+			{
+				Slots = obj.Slots;
+				return *this;
+			}
+
+			std::array<int, 5>& __GetSlots() { return Slots; }
+			int __GetSlots(int i) const { return Slots[i]; }
+			void __SetSlots(int i, int val) { Slots[i] = val; }
+			__declspec(property(get = __GetSlots, put = __SetSlots)) int Slots[];
+		};
+	}
+}
