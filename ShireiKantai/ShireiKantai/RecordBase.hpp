8c5699c6c7f4cec86ba770ec79af3c6a69d94437
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-03-08 16:26:01 +0900

    レコードの基底クラス

---
+++ ShireiKantai/ShireiKantai/RecordBase.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Siv3D.hpp>
+#include <json11.hpp>
+#include "Shigen.hpp"
+
+namespace kantai
+{
+	namespace data
+	{
+		namespace DB
+		{
+			class RecordBase
+			{
+			public:
+			};
+		}
+	}
+}
